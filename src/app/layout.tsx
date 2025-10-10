d0f4a9948d5a870deaa47e14ea2361668c60e828
Sam <109059570+Sam-nnnn@users.noreply.github.com>
2025-10-10 23:46:46 +0800

    Update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { GoogleAnalytics } from '@/features/google-analytics';
 import { generateSiteMetadata } from '@/lib/metadata/main';
 import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
