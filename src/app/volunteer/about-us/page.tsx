43056a117efa3cadf4ee01edc8b693613afa5aa5
Abby <abbysuyuyan@gmail.com>
2025-10-11 02:34:53 +0800

    Update page.tsx

--- src/app/volunteer/about-us/page.tsx
+++ src/app/volunteer/about-us/page.tsx
@@ -6,10 +6,10 @@ import { env } from "@/config/env";
 export const generateMetadata = (): Metadata => {
   return {
     title: "關於我們",
-    description: "由遠端與在地志工共同打造的資訊系統，整合地圖、需求媒合與配送支援，讓不在現場也能出一份力。",
+    description: "由資訊超人與在地志工共同打造的光復超人，整合地圖、需求媒合與小蜜蜂配送支援，讓不在現場也能出一份力。",
     openGraph: {
       title: "關於我們",
-      description: "由遠端與在地志工共同打造的資訊系統，整合地圖、需求媒合與配送支援，讓不在現場也能出一份力。",
+      description: "由資訊超人與在地志工共同打造的光復超人，整合地圖、需求媒合與小蜜蜂配送支援，讓不在現場也能出一份力。",
       url: `${env.NEXT_PUBLIC_BASE_URL}/volunteer/about-us`,
       type: "website",
     },
