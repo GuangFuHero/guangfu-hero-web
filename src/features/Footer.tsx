276efb5aea3aa256bc2dfe419210af19aa518656
BensonTW <bensonTW.chen@gmail.com>
2025-10-18 19:41:03 +0800

    add match pages back

--- src/features/Footer.tsx
+++ src/features/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Typography } from '@mui/material';
 import Link from 'next/link';
 import ReactGA from 'react-ga4';
 
@@ -9,9 +8,6 @@ export default function Footer() {
     <footer className="fixed h-[85px] bottom-0 left-0 right-0 w-full bg-[var(--light-gray-background)] text-white z-1000">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex gap-3 justify-center">
-          {/*
-          // Due to typhoon, close the matching pages temporarily
-
           <Link
             href="/resources"
             className="flex-1 max-w-[160px] bg-[var(--secondary)] hover:bg-[#166f8c] py-3 px-6 rounded-lg text-center font-bold transition-colors"
@@ -30,13 +26,6 @@ export default function Footer() {
           >
             志工媒合
           </Link>
-          */}
-
-          <Typography sx={{ textAlign: 'center' }} className="text-black">
-            因應風神颱風來襲，配合政府志工禁入令
-            <br />
-            本站 配送媒合 及 志工媒合 頁面暫不開放
-          </Typography>
         </div>
       </div>
     </footer>
